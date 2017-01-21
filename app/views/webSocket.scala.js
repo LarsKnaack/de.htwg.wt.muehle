f44ffa0e9813868ca80817b59622cf7725899070
Lars Knaack <lars.knaack@transdocu.de>
2017-01-21 15:33:58 +0100

    Moved chat to GUI interface. TODO: Remove TUI interface or replace by simple console logger

--- app/views/webSocket.scala.js
+++ app/views/webSocket.scala.js
@@ -7,29 +7,40 @@ var WS = window['MozWebSocket'] ? window['MozWebSocket'] : WebSocket;
 
 var socket = new WS('@routes.HomeController.webSocket().webSocketURL(request)');
 
+var poly_list = {};
+
 var writeMessages = function (event) {
     var json = JSON.parse(event.data);
     console.log("Got message: " + json);
     if(json["type"] === "chat") {
-        $('#socket-messages').prepend('<p>' + json["data"] + '</p>');
+        $('#chat-messages').prepend('<p>' + json["data"] + '</p>');
     } else if (json["type"] === "update") {
-        $('#tui-container').html('<pre>' + json["data"] + '</pre>');
+        var data = JSON.parse(json["data"]);
+        $.each(data, function (index, value) {
+            poly_list[index].setAttribute("color", value);
+        });
+        Polymer.updateStyles();
     }
 };
 
 $(document).ready(function() {
     socket.onmessage = writeMessages;
-    $('#tui-input').keyup(function (event) {
-        var charCode = (event.which) ? event.which : event.keyCode;
+    var gamefield = $("#gamefield");
+    for (var i = 1; i <= 24; i++) {
+        var div = document.createElement('div');
+        var poly_elem = document.createElement('morris-app');
+        poly_elem.setAttribute("color", "n");
+        poly_elem.setAttribute("vertex", i.toString());
+        poly_list[i.toString()] = poly_elem;
+        div.appendChild(poly_elem);
+        $(div).addClass("mills_fields" + i)
+            .appendTo(gamefield);
+        poly_elem.onclick = function () {
+            sendMessage("update", this.getAttribute("vertex"));
+        };
+    }
 
-        // if enter (charcode 13) is pushed, send message, then clear input field
-        if (charCode === 13) {
-            sendMessage("update", $(this).val());
-            $(this).val('');
-        }
-    });
-
-    $('#socket-input').keyup(function (event) {
+    $('#chat-input').keyup(function (event) {
         var charCode = (event.which) ? event.which : event.keyCode;
 
         // if enter (charcode 13) is pushed, send message, then clear input field
